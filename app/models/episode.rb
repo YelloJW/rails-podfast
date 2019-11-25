206d13cdf6698070af26b3759c98832e139f3c17
Joe Olley <joeolley@hotmail.co.uk>
2019-11-25 14:24:17 +0000

    updated has_many through logic

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,5 +1,5 @@
 class Episode < ApplicationRecord
-  has_many :tags, through: :episode_tag
+  has_many :tags, through: :episode_tags
   validates :title, presence: true, uniqueness: true
   validates :summary, presence: true
   validates :length, presence: true
