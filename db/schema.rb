76179428d75949bc4a7f75b860a1a6953c91d057
Doug Noble <douglasjmnoble@googlemail.com>
2019-11-25 13:56:27 +0000

    podcast episode table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_071212) do
+ActiveRecord::Schema.define(version: 2019_11_25_135543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "episodes", force: :cascade do |t|
+    t.string "title"
+    t.integer "length"
+    t.integer "upvotes"
+    t.text "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
