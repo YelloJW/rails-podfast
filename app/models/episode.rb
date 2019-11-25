b9a235eebc95a96310ef028198a77232a946a3ff
Joe Olley <joeolley@hotmail.co.uk>
2019-11-25 14:23:03 +0000

    added has many through to episode model

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,5 @@
 class Episode < ApplicationRecord
+  has_many :tags, through: :episode_tag
   validates :title, presence: true, uniqueness: true
   validates :summary, presence: true
   validates :length, presence: true
