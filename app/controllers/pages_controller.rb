9bd1e26109938ef70dc649dd00a9ff337773cb7f
Emma Bowen <emloubow@gmail.com>
2019-11-25 18:09:36 +0000

    added a form with tags

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :search]
 
   def home
   end
 
   def search
+    @default_tags = Tag.all.where(default: true)
+    @other_tags = Tag.all.where(default: false)
   end
 end
