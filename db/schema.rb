79c9ae15a26552dae78a21e0e70451170b01f07d
Joe Olley <joeolley@hotmail.co.uk>
2019-11-25 14:07:14 +0000

    added validations to episodes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_11_25_134739) do
+ActiveRecord::Schema.define(version: 2019_11_25_135543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +20,11 @@ ActiveRecord::Schema.define(version: 2019_11_25_134739) do
     t.integer "length"
     t.integer "upvotes"
     t.text "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-    create_table "tags", force: :cascade do |t|
+  create_table "tags", force: :cascade do |t|
     t.string "tag"
     t.boolean "default"
     t.datetime "created_at", null: false
