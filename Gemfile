11869a1c965f3b1ac4d9bff935d84bdfd1cd6ebb
Doug Noble <douglasjmnoble@googlemail.com>
2019-11-26 17:09:33 +0000

    seed updates & spotify url

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'pg', '~> 0.21'
 gem 'puma'
 gem 'rails', '5.2.3'
 gem 'redis'
+# Commented out until we work out how to implement Spotify / Podcasts API
+# gem 'rspotify'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module RailsPodfast
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Commented out until we work out how to implement Spotify / Podcasts API
+    # RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
