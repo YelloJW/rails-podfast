c450de5fdce9f88e6a7c6f22d768ae714787d5b2
Emma Bowen <emloubow@gmail.com>
2019-12-04 18:00:00 +0000

    random podcast generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,18 @@ class PagesController < ApplicationController
     # Commented out until we work out how to implement Spotify / Podcasts API
     # @artists = RSpotify::Artist.search('Arctic Monkeys')
     # @podcasts = RSpotify::Category.search('podcasts')
+
   end
 
   def search
     @default_tags = Tag.all.where(default: true)
     @other_tags = Tag.all.where(default: false)
+  end
+
+  def random_podcast
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js # <-- will render `app/views/reviews/create.js.erb`
+    end
   end
 end
